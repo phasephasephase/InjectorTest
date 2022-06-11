dac3f987e62cef5d7014cffed5325fb44303630b
carlton <carltonrbx@outlook.com>
2022-06-11 02:03:53 -0600

    Add project files.

---
+++ InjectorTest/Injector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+using static InjectorTest.Windows;
+
+namespace InjectorTest
+{
+    public static class Injector // yes i stole from my own launcher
+    {
+        public static async Task Inject(string path)
+        {
+            await ApplyAppPackages(path);
+
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Injecting " + path);
+                try
+                {
+                    var targetProcess = Program.Minecraft;
+                    var procHandle = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION |
+                        PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ,
+                        false, targetProcess.Id);
+
+                    var loadLibraryAddress = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+                    var allocMemAddress = VirtualAllocEx(procHandle, IntPtr.Zero,
+                        (uint)((path.Length + 1) * Marshal.SizeOf(typeof(char))), MEM_COMMIT
+                        | MEM_RESERVE, PAGE_READWRITE);
+
+                    WriteProcessMemory(procHandle, allocMemAddress, Encoding.Default.GetBytes(path),
+                        (uint)((path.Length + 1) * Marshal.SizeOf(typeof(char))), out _);
+                    CreateRemoteThread(procHandle, IntPtr.Zero, 0, loadLibraryAddress,
+                        allocMemAddress, 0, IntPtr.Zero);
+
+                    Console.WriteLine("Finished injecting");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Injection failed. Exception: " + e);
+                }
+            });
+        }
+
+        private static async Task ApplyAppPackages(string path)
+        {
+            await Task.Run(() =>
+            {
+                var infoFile = new FileInfo(path);
+                var fSecurity = infoFile.GetAccessControl();
+                fSecurity.AddAccessRule(
+                    new FileSystemAccessRule(new SecurityIdentifier("S-1-15-2-1"),
+                    FileSystemRights.FullControl, InheritanceFlags.None,
+                    PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+
+                infoFile.SetAccessControl(fSecurity);
+            });
+            
+            Console.WriteLine("Applied ALL_APPLICATION_PACKAGES permission to " + path);
+        }
+    }
+}
---
+++ InjectorTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InjectorTest
+{
+    internal class Program
+    {
+        public static Process Minecraft;
+
+        public static Task Main(string[] args) => new Program().MainAsync(args);
+
+        private async Task MainAsync(string[] args)
+        {
+            if (args.Length == 0 || args.Length > 1 || !args[0].EndsWith(".dll"))
+            {
+                Console.WriteLine("Provide a path to a DLL file (use \"quotes\" if you have to)");
+                Environment.Exit(-1);
+            }
+
+            var minecraftIndex = Process.GetProcessesByName("Minecraft.Windows");
+
+            if (minecraftIndex.Length != 0)
+            {
+                Console.WriteLine("Minecraft is already open, skipping module check");
+                Minecraft = minecraftIndex[0];
+                await Injector.Inject(args[0]);
+            }
+            else
+            {
+                Process.Start("minecraft://");
+                Minecraft = Process.GetProcessesByName("Minecraft.Windows")[0];
+                await WaitForModules();
+                await Injector.Inject(args[0]);
+            }
+
+            Console.WriteLine("done");
+        }
+
+        private async Task WaitForModules()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Waiting for Minecraft to load");
+                while (true)
+                {
+                    Minecraft.Refresh();
+                    if (Minecraft.Modules.Count > 160) break;
+                    Thread.Sleep(4000);
+                }
+            });
+            Console.WriteLine("Minecraft finished loading");
+        }
+    }
+}
